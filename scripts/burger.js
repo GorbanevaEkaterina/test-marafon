ac93971b0b8b4f1916c16a8c0ce44189c3fa83d7
GorbanevaEkaterina <105439408+GorbanevaEkaterina@users.noreply.github.com>
2024-11-21 14:36:32 +0400

    fix:burger code style

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burger = document.getElementById('burger');
-    const menu = document.getElementById('menu');
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.getElementById("burger");
+  const menu = document.getElementById("menu");
 
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('open');
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("open");
 
-        if (menu.classList.contains('open')) {
-            menu.classList.remove('open');
-            setTimeout(() => {
-                menu.style.display = 'none';
-            }, 300);
-        } else {
-            menu.style.display = 'block';
-            setTimeout(() => {
-                menu.classList.add('open');
-            }, 10);
-        }
-    });
+    if (menu.classList.contains("open")) {
+      menu.classList.remove("open");
+      setTimeout(() => {
+        menu.style.display = "none";
+      }, 300);
+    } else {
+      menu.style.display = "block";
+      setTimeout(() => {
+        menu.classList.add("open");
+      }, 10);
+    }
+  });
 });
